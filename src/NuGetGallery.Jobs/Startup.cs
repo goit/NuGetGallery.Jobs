6939687825047b99fec2974dd54abc4199ec6894
Jozef Izso <jozef.izso@gmail.com>
2016-03-20 19:14:10 +0100

    Load SQL Server connection string from configuration data.

--- src/NuGetGallery.Jobs/Startup.cs
+++ src/NuGetGallery.Jobs/Startup.cs
@@ -7,10 +7,33 @@ using Microsoft.Extensions.DependencyInjection;
 using Hangfire;
 using Hangfire.SqlServer;
 
+using Microsoft.Extensions.Configuration;
+
 namespace NuGetGallery.Jobs
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,7 +47,7 @@ namespace NuGetGallery.Jobs
             app.UseIISPlatformHandler();
             app.UseStaticFiles();
 
-            SqlServerStorage sjs = new SqlServerStorage("Server=(localdb)\\mssqllocaldb;Database=TestDatabase;Trusted_Connection=True;MultipleActiveResultSets=true");
+            SqlServerStorage sjs = new SqlServerStorage(Configuration["Data:DefaultConnection:ConnectionString"]);
             JobStorage.Current = sjs;
             BackgroundJobServerOptions backgroundJobServerOptions = new BackgroundJobServerOptions()
             {
